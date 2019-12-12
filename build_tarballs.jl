006ec85d315ce10ff7f59ca0c38d92bc656febc2
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2019-12-12 22:37:04 +0100

    Modified platforms statement

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -28,31 +28,29 @@ $CC -DTRILIBRARY -fPIC -DNDEBUG -DNO_TIMER -DEXTERNAL_TEST $LDFLAGS --shared -o 
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
+# platforms = [
+#     Linux(:i686, libc=:glibc),
+#     Linux(:x86_64, libc=:glibc),
+#     Linux(:aarch64, libc=:glibc),
+#     Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+#     Linux(:powerpc64le, libc=:glibc),
+#     Linux(:i686, libc=:musl),
+#     Linux(:x86_64, libc=:musl),
+#     Linux(:aarch64, libc=:musl),
+#     Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+#     MacOS(:x86_64),
+#     FreeBSD(:x86_64),
+#     Windows(:i686),
+#     Windows(:x86_64)
+# ]
+
+platforms = BinaryBuilder.supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libtriangle", :libtriangle)
-]
+products(prefix) = [  LibraryProduct(prefix, "libtriangle", :libtriangle)]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-    
-]
+dependencies = []
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
